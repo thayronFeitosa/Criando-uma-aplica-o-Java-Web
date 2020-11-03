d0ccf33b76a85cd66dba39f76b7c01160ca43fae
thayron <thayronf3@gmail.com>
2020-11-03 02:39:01 -0300

    Foi iniciado o projeto e configurado as rotas para buscar o arquivo .html referente a sua rota

---
+++ src/main/java/com/eventoapp/demo/IndexController.java
@@ -0,0 +1,16 @@
+package com.eventoapp.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	
+	public String index() {
+		return "index";
+	}
+
+}
